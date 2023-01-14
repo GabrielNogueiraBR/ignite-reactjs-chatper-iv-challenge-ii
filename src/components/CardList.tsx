4940d764c3421eb5adb936f81f8bc3829f440155
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-01-14 15:21:40 -0300

    feat: update CardList Component

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { Grid, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -17,16 +17,30 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [imageModalUrl, setImageModalUrl] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (imageUrl: string): void => {
+    setImageModalUrl(imageUrl);
+    onOpen();
+  };
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <Grid gap={10} templateColumns="repeat(3, 1fr)">
+        {cards.map(card => (
+          <Card data={card} viewImage={handleViewImage} />
+        ))}
+      </Grid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        imgUrl={imageModalUrl}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </>
   );
 }
